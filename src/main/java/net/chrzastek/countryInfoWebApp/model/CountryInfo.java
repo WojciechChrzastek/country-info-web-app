a0d9c284587088e6ab3e91c966896521c82c4bd7
Wojciech Chrzastek <w.chrzastek@gmail.com>
2020-07-04 16:39:42 +0200

    Create fields mapping db columns

--- src/main/java/net/chrzastek/countryInfoWebApp/model/CountryInfo.java
+++ src/main/java/net/chrzastek/countryInfoWebApp/model/CountryInfo.java
@@ -8,6 +8,12 @@ public class CountryInfo {
   private static final String DB_NAME = "country-info.db";
   private static final String TABLE_NAME = "ipNumbers";
 
+  private Integer id;
+  private Boolean isProcessed;
+  private String processedTimestamp;
+  private String ip;
+  private String countryName;
+
   public static void main(String[] args) {
     DbHandler dbHandler = new DbHandler();
     Connection conn = dbHandler.connectToNewDatabase(DB_NAME);
