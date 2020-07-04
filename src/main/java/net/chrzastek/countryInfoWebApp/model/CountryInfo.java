2e4b3ca8e8d8b73786fdde835eb4b5898ab4249f
Wojciech Chrzastek <w.chrzastek@gmail.com>
2020-07-04 16:41:52 +0200

    Generate getters, setters and constructor for created fields

--- src/main/java/net/chrzastek/countryInfoWebApp/model/CountryInfo.java
+++ src/main/java/net/chrzastek/countryInfoWebApp/model/CountryInfo.java
@@ -19,4 +19,52 @@ public class CountryInfo {
     Connection conn = dbHandler.connectToNewDatabase(DB_NAME);
     dbHandler.createNewTable(TABLE_NAME, conn);
   }
+
+  public CountryInfo(Integer id, Boolean isProcessed, String processedTimestamp, String ip, String countryName) {
+    this.id = id;
+    this.isProcessed = isProcessed;
+    this.processedTimestamp = processedTimestamp;
+    this.ip = ip;
+    this.countryName = countryName;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public Boolean getProcessed() {
+    return isProcessed;
+  }
+
+  public void setProcessed(Boolean processed) {
+    isProcessed = processed;
+  }
+
+  public String getProcessedTimestamp() {
+    return processedTimestamp;
+  }
+
+  public void setProcessedTimestamp(String processedTimestamp) {
+    this.processedTimestamp = processedTimestamp;
+  }
+
+  public String getIp() {
+    return ip;
+  }
+
+  public void setIp(String ip) {
+    this.ip = ip;
+  }
+
+  public String getCountryName() {
+    return countryName;
+  }
+
+  public void setCountryName(String countryName) {
+    this.countryName = countryName;
+  }
 }
