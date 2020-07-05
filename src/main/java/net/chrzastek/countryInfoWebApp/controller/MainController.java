58366bc7e8499cc1f54ee71814151019f4cf6a38
Wojciech Chrzastek <w.chrzastek@gmail.com>
2020-07-06 01:05:51 +0200

    Add and modify request mappings

--- src/main/java/net/chrzastek/countryInfoWebApp/controller/MainController.java
+++ src/main/java/net/chrzastek/countryInfoWebApp/controller/MainController.java
@@ -1,6 +1,7 @@
 package main.java.net.chrzastek.countryInfoWebApp.controller;
 
 import main.java.net.chrzastek.countryInfoWebApp.dao.CountryInfoDao;
+import main.java.net.chrzastek.countryInfoWebApp.database.DbHandler;
 import main.java.net.chrzastek.countryInfoWebApp.model.CountryInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.sql.Connection;
 import java.util.List;
 
 @Controller
@@ -18,7 +20,7 @@ public class MainController {
   private CountryInfoDao countryInfoDao;
 
   @RequestMapping(value = "/")
-  public ModelAndView listContact(ModelAndView model) {
+  public ModelAndView listCountry(ModelAndView model) {
     List<CountryInfo> countryInfoList = countryInfoDao.showReport();
     model.addObject("countryInfoList", countryInfoList);
     model.setViewName("index");
@@ -26,19 +28,52 @@ public class MainController {
     return model;
   }
 
-  @RequestMapping(value = "/new", method = RequestMethod.GET)
-  public ModelAndView addIp(ModelAndView model) {
-    CountryInfo countryInfo = new CountryInfo();
-    model.addObject("countryInfo", countryInfo);
-    model.setViewName("countryInfo_form");
+  @RequestMapping(value = "/report")
+  public ModelAndView report(ModelAndView model) {
+    List<CountryInfo> countryInfoList = countryInfoDao.showReport();
+    model.addObject("countryInfoList", countryInfoList);
+    model.setViewName("report");
 
     return model;
   }
 
-  @RequestMapping(value = "/save", method = RequestMethod.POST)
+  @RequestMapping(value = "/update")
+  public ModelAndView update(ModelAndView model) {
+
+    DbHandler dbHandler = new DbHandler();
+    Connection conn = dbHandler.connectToNewDatabase("country-info.db");
+    DbHandler.updateAllRows(conn, dbHandler);
+
+    model.setViewName("update");
+    return model;
+  }
+
+  @RequestMapping(value = "/add", method = RequestMethod.GET)
+  public ModelAndView add(ModelAndView model) {
+    CountryInfo countryInfo = new CountryInfo();
+    model.addObject("countryInfo", countryInfo);
+    model.setViewName("addIpForm");
+
+    return model;
+  }
+
+  @RequestMapping(value = "/addIp", method = RequestMethod.POST)
   public ModelAndView addIp(@ModelAttribute CountryInfo countryInfo) {
     countryInfoDao.addIpNumber(countryInfo);
 
     return new ModelAndView("redirect:/");
   }
+
+  @RequestMapping(value = "/initial")
+  public ModelAndView initial(ModelAndView model) {
+
+    DbHandler dbHandler = new DbHandler();
+    Connection conn = dbHandler.connectToNewDatabase("country-info.db");
+    List<String> initialIpList = DbHandler.selectInitialIp(conn);
+
+    model.addObject("initialIpList", initialIpList);
+    model.setViewName("initial");
+    return model;
+  }
+
 }
