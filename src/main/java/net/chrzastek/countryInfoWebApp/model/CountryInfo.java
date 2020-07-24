98b6c56e8f9a69c2eb2b4f43b061f43233715cdb
Wojciech Chrzastek <w.chrzastek@gmail.com>
2020-07-24 14:35:46 +0200

    Delete unnecessary code

--- src/main/java/net/chrzastek/countryInfoWebApp/model/CountryInfo.java
+++ src/main/java/net/chrzastek/countryInfoWebApp/model/CountryInfo.java
@@ -1,20 +1,12 @@
 package main.java.net.chrzastek.countryInfoWebApp.model;
 
 public class CountryInfo {
-//  private static final String DB_NAME = "country-info.db";
-//  private static final DbHandler dbHandler = new DbHandler();
-//  private static final String TABLE_NAME = "ipNumbers";
 
   private Integer id;
   private Boolean isProcessed;
   private String processedTimestamp;
   private String ip;
   private String countryName;
-
-//  public static void main(String[] args) {
-//    Connection conn = dbHandler.connectToNewDatabase(DB_NAME);
-//    dbHandler.createNewTable(TABLE_NAME, conn);
-//  }
 
   public CountryInfo(Integer id, Boolean isProcessed, String processedTimestamp, String ip, String countryName) {
     this.id = id;
